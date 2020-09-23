4252e3974e6046474f88de01709232e8cb8872de
iamarjun <arjunmanoj1995@gmail.com>
2020-09-24 00:02:26 +0530

    change light color to the shade of 50

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xFFF44336);
-const kPrimaryColorLight = Color(0xFFFFCDD2);
+const kPrimaryColorLight = Color(0xFFFFEBEE);
 const kPrimaryTextColor = Color(0xFFFFFFFF);
